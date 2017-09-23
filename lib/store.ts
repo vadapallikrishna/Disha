4f14d6db2ac5091ee91ed5a4a3ce38d82ceab18c
krishna vadapalli <vadapallikrishna@gmail.com>
2017-09-23 15:44:00 +0530

    simple state mg

--- lib/store.ts
+++ lib/store.ts
@@ -1,22 +1,37 @@
-import * as request from 'superagent';  
+import * as request from 'superagent';
+import * as React from 'react';
 
 interface Action{
     name: string,
     handler: (req: request.SuperAgentStatic)=>Object
 }
 
-
 class Store{
     actions: Action[]     
-    state: object[]
+    state: object
+    
 
     perform(actionName: string){
         let action = this.actions.find((a: Action)=> a.name == actionName)
-        if (action != undefined)
-            this.state.push(Object.assign(this.state[this.state.length-1],action.handler(request)));
+        if (action != undefined){
+            Object.assign(this.state,action.handler(request));
+            
+        }
+
     }
 
+    register(actionName: string, handler: (req: request.SuperAgentStatic)=> Object){
+        this.actions.push({name: actionName,handler: handler})
+    }
 
 }
 
-export {Action, Store}
+
+function connectToStore(store: Store,component: any){
+    if(component instanceof React.Component){
+        setInterval(()=>component.setState(store.state[Object.keys(component.state)[0]]))
+    }
+}
+
+
+export {Action, Store, connectToStore}
